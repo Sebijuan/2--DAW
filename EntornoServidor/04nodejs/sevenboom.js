8ca1213434a0ad193d129eb1e0b26e49e0fe7a64
Sebijuan <sjuacoc2t@adaits.es>
2024-10-01 02:18:51 +0200

    Update sevenboom.js

--- EntornoServidor/04nodejs/sevenboom.js
+++ EntornoServidor/04nodejs/sevenboom.js
@@ -1,9 +1,12 @@
-for(let i = 1; i<=100; i++ ){
-
-if ( i % 7 === 0){
-    console.log("Boom");
-}else{
-    console.log(i);
+function sevenBoom() {
+    for (let i = 1; i <= 100; i++) {
+        if (i.toString().includes('7')) {
+            console.log("Boom");
+        } else {
+            console.log(i);
+        }
+    }
 }
 
-}
+
+sevenBoom();
