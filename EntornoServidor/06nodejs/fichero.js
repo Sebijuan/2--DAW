ae32abf716cf79f37a9d44fe61e5aa5169d65a84
Sebijuan <sjuacoc2t@adaits.es>
2024-10-15 20:11:06 +0200

    actualizacion

---
+++ EntornoServidor/06nodejs/fichero.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+
+if (process.argv.length < 3) {
+    console.log('Por favor, proporciona el nombre del archivo como argumento.');
+    process.exit(1); 
+}
+
+
+const fileName = process.argv[2];
+
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+        console.error(`Error al leer el archivo: ${err.message}`);
+        process.exit(1);
+    }
+    
+   
+    console.log(data);
+});
