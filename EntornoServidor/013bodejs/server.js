6a3298c802b9234b331865045c6c3ab4d3f70b85
Sebijuan <sjuacoc2t@adaits.es>
2024-11-11 19:49:59 +0100

    Update server.js

--- EntornoServidor/013bodejs/server.js
+++ EntornoServidor/013bodejs/server.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.get('/page', (req, res) => {
     const name = req.query.name;
 
-    
+   
     fs.readFile(path.join(__dirname, 'views', 'page.html'), 'utf-8', (err, data) => {
         if (err) {
             res.status(500).send("Error al cargar la página");
@@ -26,7 +26,30 @@ app.get('/error', (req, res) => {
 });
 
 
+app.get('/fizzbuzz', (req, res) => {
+    const number = parseInt(req.query.number, 10);
+
+    if (isNaN(number) || number < 1) {
+        return res.status(400).send("Por favor, proporciona un número válido mayor a 0 en la query string.");
+    }
+
+    const fizzBuzzArray = [];
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzBuzzArray.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            fizzBuzzArray.push("Fizz");
+        } else if (i % 5 === 0) {
+            fizzBuzzArray.push("Buzz");
+        } else {
+            fizzBuzzArray.push(i);
+        }
+    }
+
+    res.send(fizzBuzzArray.join(', '));
+});
+
+
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
-
