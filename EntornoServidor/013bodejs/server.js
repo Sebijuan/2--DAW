ac3405cb12b944f90065e268c87ce5488ec52a40
Sebijuan <sjuacoc2t@adaits.es>
2024-11-11 19:47:16 +0100

    actualizacion

--- EntornoServidor/013bodejs/server.js
+++ EntornoServidor/013bodejs/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Ruta /page: Devuelve el archivo page.html
+
 app.get('/page', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'page.html'));
+    const name = req.query.name;
+
+    
+    fs.readFile(path.join(__dirname, 'views', 'page.html'), 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send("Error al cargar la página");
+        } else {
+            
+            const pageContent = name ? data.replace('${name}', `Hello, ${name}!`) : data.replace('${name}', 'Bienvenido a la Página Principal');
+            res.send(pageContent);
+        }
+    });
 });
 
-// Ruta /error: Devuelve el archivo error.html con código 404
+
 app.get('/error', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 });
 
-// Iniciar el servidor
+
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
+
